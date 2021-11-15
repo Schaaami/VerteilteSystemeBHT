c3f97f2adfd6ae69e3988b579f52018078646453
Anserjan <aj@anserjan.de>
2021-11-15 18:53:28 +0100

    take user input and convert to byte

--- Aufgabe2/client.py
+++ Aufgabe2/client.py
@@ -6,8 +6,9 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    input = int(input())
     s.connect((HOST, PORT))
-    s.sendall(b'Hellxy<yxcvyxvydasdasdscxyvvxyvsdaaaaaaaaaaaaaaao, world')
+    s.sendall(input.to_bytes(input.bit_length(), 'big'))
     data = s.recv(1024)
 
 print('Received', repr(data))
